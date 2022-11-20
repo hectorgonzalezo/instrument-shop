57b2face846c2c4032938e0ed3a4b468101c6f02
Hector Gonzalez <gorozco.hector@gmail.com>
2022-11-20 16:46:48 -0600

    Add instrument update

--- controllers/instrumentController.js
+++ controllers/instrumentController.js
@@ -69,7 +69,7 @@ exports.instrument_create_post = [
     .optional({ checkFalsy: true })
     .trim()
     .escape()
-    .isLength({ max: 100 })
+    .isLength({ max: 300 })
     .withMessage("Instrument description must be at most 100 characters long"),
   body("tuning")
     .trim(),
@@ -90,8 +90,6 @@ exports.instrument_create_post = [
       const errors = validationResult(req);
       // get chosen fields
       let categoriesArray = Object.keys(req.body).filter((field) => /.*-category/.test(field));
-      console.log(categoriesArray.length);
-      console.log(errors.array())
   
       // If there are any errors, rerender form with previous values and error messages
       // or if no category is chosen
@@ -143,6 +141,8 @@ exports.instrument_create_post = [
     }
 ]
 
+// Update a instrument
+// shows instrument create page and populates it with previous category data
 exports.instrument_update_get = (req, res, next) => {
   async.parallel(
     {
@@ -157,7 +157,6 @@ exports.instrument_update_get = (req, res, next) => {
         if (err) {
           return next(err);
         }
-
         res.render("instruments_create", {
           title: "Update instrument",
           instrument: results.instrument,
@@ -167,9 +166,119 @@ exports.instrument_update_get = (req, res, next) => {
   )
 };
 
-exports.instrument_update_post = (req, res) => {
-  res.send("Instrument update post")
-};
+// Similar to create_post, but finds the category by id before updating it
+exports.instrument_update_post = [
+body("name", "Instrument name required")
+  .trim()
+  .escape()
+  .isLength({ min: 3, max: 25 })
+  .withMessage("Instrument name must be between 3 and 25 characters long")
+  .isAlpha()
+  .withMessage("Instrument name can only contain letters or numbers"),
+body("brand", "Instrument brand required")
+  .trim()
+  .escape()
+  .isLength({ min: 3, max: 25 })
+  .withMessage("Instrument brand must be between 3 and 25 characters long")
+  .isAlpha()
+  .withMessage("Instrument brand can only contain letters or numbers"),
+body("model")
+  .optional({ checkFalsy: true })
+  .trim()
+  .escape()
+  .isAlphanumeric()
+  .withMessage("Instrument model can only contain letters or numbers"),
+body("description", "Instrument description required")
+  .optional({ checkFalsy: true })
+  .trim()
+  .escape()
+  .isLength({ max: 300 })
+  .withMessage("Instrument description must be at most 100 characters long"),
+body("tuning")
+  .trim(),
+body("price")
+  .trim()
+  .escape()
+  .toInt()
+  .isInt({ min: 1 })
+  .withMessage("Price should be at least 1"),
+body("stock")
+  .trim()
+  .escape()
+  .toInt()
+  .isInt({ min: 1 })
+  .withMessage("Stock should be at least 1"),
+  (req, res, next) =>{
+    // Get validations
+    const errors = validationResult(req);
+    // get chosen fields
+    let categoriesArray = Object.keys(req.body).filter((field) =>
+      /.*-category/.test(field)
+    );
+
+    // If there are any errors, rerender form with previous values and error messages
+    // or if no category is chosen
+    if (!errors.isEmpty() || categoriesArray.length < 1) {
+      const noCategoryError = { msg: "Choose at least one category" };
+
+      Category.find().exec((err, categories) => {
+        res.render("instruments_create", {
+          title: "Create new instrument",
+          instrument: req.body,
+          categories,
+          errors:
+            categoriesArray.length < 1
+              ? [...errors.array(), noCategoryError]
+              : errors.array(),
+        });
+      });
+      return;
+    }
+
+    // Make an array full of categories
+
+    categoriesArray = categoriesArray.map(
+      (category) => category.match(/.*(?=-category)/)[0]
+    );
+    // look for chosen categories, add them to array and then save instrument
+    Category.find({ name: { $in: categoriesArray } }).exec(
+      (err, categories) => {
+        if (err) {
+          return next(err);
+        }
+        const categoriesIds = categories.map((category) => category._id);
+
+        // If it's valid, add the category to database and open record
+        const instrument = new Instrument({
+          name: req.body.name,
+          brand: req.body.brand,
+          ...(req.body.model !== "" && { model: req.body.model }),
+          ...(req.body.description !== "" && {
+            description: req.body.description,
+          }),
+          ...(req.body.tuning !== "" && { tuning: req.body.tuning }),
+          categories: categoriesIds,
+          price: req.body.price,
+          stock: req.body.stock,
+          _id: req.params.id,
+        });
+        
+        // update instrument
+        Instrument.findByIdAndUpdate(
+          req.params.id,
+          instrument,
+          {},
+          (instrumentErr, updatedInstrument) => {
+            if (err) {
+              return next(instrumentErr);
+            }
+            res.redirect(updatedInstrument.url);
+          }
+        );
+      }
+    );
+  }
+];
 
 exports.instrument_delete_get = (req, res) => {
   res.send("Instrument delete get")
